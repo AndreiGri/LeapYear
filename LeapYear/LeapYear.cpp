40d4e1a15adaa8e436b4531000145cabfe19b38e
Andrei Gridin <andreigridin918@gmail.com>
2023-09-19 19:48:40 +0500

    Добавьте файлы проекта.

---
+++ LeapYear/LeapYear.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменение кодировки терминала:
+    system("chcp 1251 > nul");
+
+    int year = 0;
+
+    cout << endl;
+    cout << " Введите год в формате ГГГГ: ";
+    cin >> year;
+
+    if ((year % 100 == 0 && year % 400 != 0) || year % 4 != 0) {
+        cout << " В году 365 дней (не високосный)!!!" << endl;
+    }
+    else {
+        cout << " В году 364 дня (високосный)!!!" << endl;
+    }
+
+    // Задержка окна консоли:
+    system("pause > nul");
+
+    return 0;
+}
